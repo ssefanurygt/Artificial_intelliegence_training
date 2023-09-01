844d531c26e5e778657491f25f95a76f7286094f
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-09-01 17:04:46 +0300

    Add files via upload

---
+++ PythonApplication1string/PythonApplication1string.py
@@ -0,0 +1,26 @@
+#print("merhaba kahramanmaras")
+#print('ali\'nin evi')
+#print("merhaba\ndunya")
+#print("merhaba\t\t\tdunya")
+
+mesaj = "merhaba"
+mesaj2 = "dunya"
+isim= "yusuf"
+yas= "48"
+#print(mesaj2 + mesaj)
+##print(mesaj[2])
+#print(mesaj2[-3])
+#print(mesaj[0:4])
+#print(mesaj2[::2])
+#print(mesaj2[::-2])
+#mesaj= mesaj.upper()
+#mesaj2= mesaj.lower()
+#print(mesaj2)
+#mesaj=mesaj.capitalize()
+#print(mesaj)
+#print(mesaj.startswith("me"))  #mesaj degiskeninin me ile baslayip baslamadigini kontrol eder.
+#print(mesaj2.endswith("kl"))
+#print(len(mesaj))
+#print(mesaj*6)
+print("{} {} yasindadir".format( isim ,yas))
+print(f"({isim} {yas} yasindadir)")
