267b3e6bd5a5387c6b4b76e70bce9f8b8d9057e6
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-09-01 17:14:22 +0300

    Update PythonApplication1string.py

--- PythonApplication1string/PythonApplication1string.py
+++ PythonApplication1string/PythonApplication1string.py
@@ -13,14 +13,20 @@ yas= "48"
 #print(mesaj[0:4])
 #print(mesaj2[::2])
 #print(mesaj2[::-2])
+
 #mesaj= mesaj.upper()
 #mesaj2= mesaj.lower()
+
 #print(mesaj2)
+
 #mesaj=mesaj.capitalize()
+
 #print(mesaj)
+
 #print(mesaj.startswith("me"))  #mesaj degiskeninin me ile baslayip baslamadigini kontrol eder.
 #print(mesaj2.endswith("kl"))
 #print(len(mesaj))
 #print(mesaj*6)
+
 print("{} {} yasindadir".format( isim ,yas))
 print(f"({isim} {yas} yasindadir)")
