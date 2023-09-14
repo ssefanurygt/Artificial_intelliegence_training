94e5ad4b88af245ac4ae1a25f4614421447abaf9
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-09-14 14:25:36 +0300

    Add files via upload

---
+++ pozitif_bölen_bulan_program/pozitif_bölen_bulma.py
@@ -0,0 +1,12 @@
+﻿sayı=int(input("sayı giriniz :"))
+bölen_sayısı=0
+ 
+for i in range(1,sayı+1):
+    if sayı%i == 0:
+        bölen_sayısı += 1
+
+print(f"{sayı} sayısının {bölen_sayısı} kadar böleni vardır")
+
+
+
+
