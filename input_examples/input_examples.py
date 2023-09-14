2c9b02e0151918a870d7dd53de0ac9c4bfff92d2
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-09-14 14:22:43 +0300

    Add files via upload

---
+++ input_examples/input_examples.py
@@ -0,0 +1,18 @@
+﻿sayı=input("bir sayı giriniz") 
+print(sayı) #ekrandan alınan sayılar otomatik string algılanır,sayılarla işlem yapmak için int'e çevirmek gerekir.
+
+
+sayı=int(sayı) #ya da int(input("sayı giriniz)) şeklinde çevrilir
+print(type(sayı))
+
+faktorıyel= 1
+
+for i in range(1, sayı + 1):
+    faktorıyel*=i
+    print(faktorıyel)
+    
+i=2
+while i<=sayı:
+        faktorıyel*=i
+        i+=1
+        print(faktorıyel)
