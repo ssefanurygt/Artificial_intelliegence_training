2c1e38b63bd0fb17e52ae8af7ca3f88a3a1cbe49
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-09-15 14:32:47 +0300

    Add files via upload

---
+++ asalmi_example/asalmi_examples.py
@@ -0,0 +1,10 @@
+﻿sayı=int(input("sayı giriniz"))
+prime=True #girilen sayıyı en başta asal kabul ettik sonra sayıdan küçük sayılara bölünürlüğünü kontrol ettik
+for i in range(2,sayı):
+    if sayı%i == 0:
+        prime=False
+        break
+if prime==True:
+    print(f"{sayı} sayısı asaldır")
+else:
+    print(f"{sayı} sayısı asal değildir")
