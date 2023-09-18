72519040c94654dc57ab1da672a9661b4d8ba5aa
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-09-18 12:39:06 +0300

    Add files via upload

---
+++ sayının_rakamları_toplamını_bulan_program/sayının_rakamları_toplamını_bulan_program.py
@@ -0,0 +1,16 @@
+﻿sayı=int(input("sayı giriniz :"))
+toplam=0
+geçici_sayı=sayı
+str_sayı=str(sayı)
+for rakam in str_sayı:   #stringe çevirerek yapma
+    toplam+=int(rakam)
+    print(toplam)
+    
+
+
+while geçici_sayı !=0:
+    basamak=geçici_sayı%10  #alternatif çözüm
+    toplam+=basamak
+    geçici_sayı //=10 #tam sayı bölmesi
+    print(toplam)
+    
