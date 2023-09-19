0e2bea5bc6ca4fe5daf6f26c8d2eeeb92339e31f
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-09-19 20:56:19 +0300

    Add files via upload

---
+++ hangi_harfin_kaç_kere_kullanıldığını_gösteren_program/hangi_harfin_kaç_kere_kullanıldığını_gösteren_program.py
@@ -0,0 +1,26 @@
+﻿metin=input("metni giriniz :")
+
+sözlük=dict()
+
+for harf in metin:
+    if harf in sözlük:
+        sözlük[harf] += 1
+    else:
+        sözlük[harf]=1
+        for harf,adet in sözlük.items():
+               print(harf,adet)
+            
+        
+#ekrandan okunan bir metinde a harflerini büyük yapan bir program yazınız.
+
+metin=input("bir metin giriniz :")
+metin2=""
+for harf in metin:
+    if harf=="a":
+        metin2+= "A"  #stringlerdeki toplama işlemi yanına yazmak demek.
+    else:
+        metin2+= harf
+    print(metin2)
+         
+
+        
