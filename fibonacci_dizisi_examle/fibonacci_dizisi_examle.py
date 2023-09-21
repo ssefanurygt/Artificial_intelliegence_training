adeaf72cca5f4c08a33ca7d2ee862a7aeb01cf1e
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-09-21 13:31:57 +0300

    Add files via upload

---
+++ fibonacci_dizisi_examle/fibonacci_dizisi_examle.py
@@ -0,0 +1,23 @@
+﻿#ilk 100 fibonacci dizisini ekrana yazdırınız.
+
+fibonacci_list =[]
+fibonacci_list.append(1)
+fibonacci_list.append[1]
+
+index=2
+while True:
+    fibonacci_list.append(fibonacci_list[index-2]+ fibonacci_list[index-1])
+    index+=1
+    if len(fibonacci_list)==100:
+        break
+   
+
+    #for döngüsüyle yapma
+    fibonacci_list =[]
+fibonacci_list.append(1)
+fibonacci_list.append[1]
+     
+for i in range(2,100):
+    fibonacci_list.append(fibonacci_list[index-2]+ fibonacci_list[index-1])
+    
+    print(fibonacci_list)
