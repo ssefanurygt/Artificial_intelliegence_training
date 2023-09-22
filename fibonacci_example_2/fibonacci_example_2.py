5cb94390cee811edb12aa7b88d45f9d87ed292b0
sefanur <122383644+ssefanurygt@users.noreply.github.com>
2023-09-22 22:01:31 +0300

    Add files via upload

---
+++ fibonacci_example_2/fibonacci_example_2.py
@@ -0,0 +1,15 @@
+﻿#100 basasmaklı ilk fibonacci sayısını yazdırın.
+fibonacci_list=[]
+fibonacci_list.append(1)
+fibonacci_list.append(1)
+
+index =2
+while True:
+    fibonacci_list.append(fibonacci_list[index-2]+fibonacci_list[index-1])
+    terim=(fibonacci_list[index-2]+fibonacci_list[index-1])
+    if len(str(fibonacci_list))==100:
+        print(terim)
+        print(index)
+        break
+    index+=1
+   
